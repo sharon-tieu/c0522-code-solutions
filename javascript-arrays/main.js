dc4a5c943ed8a94c7052df3a8b5cca4c6cc6bdac
sharon-tieu <tieusharon@gmail.com>
2022-05-17 17:15:14 +0000

    Updated.

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -2,8 +2,8 @@ var colors = ['red', 'white', 'blue'];
 console.log('value of colors[0]:', colors[0]);
 console.log('value of colors[1]:', colors[1]);
 console.log('value of colors[2]:', colors[2]);
-console.log('American is ' + colors[0] + ',' + ' ' + colors[1] + ',' + ' ' + 'and' + ' ' + colors[2] + '.');
-colors[2] = ['green'];
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', ' + 'and ' + colors[2] + '.');
+colors[2] = 'green';
 console.log('Mexico is ' + colors[0] + ',' + ' ' + colors[1] + ',' + ' ' + 'and' + ' ' + colors[2] + '.');
 console.log(colors);
 
