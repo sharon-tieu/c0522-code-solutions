543efdb866950c77448128b57565a67cfde6baed
sharon-tieu <tieusharon@gmail.com>
2022-05-24 14:48:49 +0000

    Updated getVowels.

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -5,7 +5,11 @@
 // function isVowel(char) { }
 // include the string 'aeiouAEIOU' to include all of the vowels that are lower and uppercased.
 // .includes() function will return true or false if char matches any of the strings so then write: // return 'aeiouAEIOU'.includes(char)
+// ^^ old method
 
+// Set.prototype.has() method returns a boolean to indicate whether an element with the specified value exists in the Set object or not.
+// In this case, we will set Set() to 'AEIOU' and 'aeiou' then assign it a new variable for it to be returned.
 function isVowel(char) {
-  return 'aeiouAEIOU'.includes(char);
+  var vowels = new Set('AaEeIiOoUu');
+  return vowels.has(char);
 }
