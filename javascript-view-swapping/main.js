2cc1bb720b0b42049e54b9e404411a076d46d69b
sharon-tieu <tieusharon@gmail.com>
2022-05-27 17:41:59 +0000

    In progress.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,6 +1,14 @@
-var $tabs = document.querySelector('.tab');
-// var $views = document.querySelector('.view');
-var $tabContainer = document.querySelector('.tab-container');
+var $tabs = document.querySelectorAll('.tab'); // selecting all tab class
+var $views = document.querySelectorAll('.view'); // selecting all view class
+var $tabContainer = document.querySelector('.tab-container'); // selecting tab-container class
+
+// create a 'click eventListener to '.tab'
+// create a function:
+//   if event.target .matches() with '.tab'
+//   create a for loop to go through each $tabs so:
+//     for (var i = 0; i < $tabs.length; i++)
+//   IF $tabs at index equals to event.target, then change the class name to 'active'
+//   ELSE if $tabs at index does NOT equals to event.target then the class name will NOT update and remain as 'tab'.
 
 $tabContainer.addEventListener('click', function (event) {
   if (event.target.matches('.tab')) {
@@ -12,4 +20,6 @@ $tabContainer.addEventListener('click', function (event) {
       }
     }
   }
+  var $dataViewValue = event.target.getAttribute('data-view');
+  // begin for loop
 });
