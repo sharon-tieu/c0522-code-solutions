12d036bdecd24b3f741e523320faaccf390a9200
sharon-tieu <tieusharon@gmail.com>
2022-05-27 17:03:51 +0000

    in progress.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,5 +1,5 @@
 // var $taskList = document.querySelector('.task-list');
-// document.addEventListener('click', function (event) {
+//  document.addEventListener('click', function (event) {
 //  console.log('event.target:', event.target);
 //  console.log('event.target.tagName:', event.target.tagName);
 // }
