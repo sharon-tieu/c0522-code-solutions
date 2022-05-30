6f648280a6bcacc7ac843b3def0a0f72704b9408
sharon-tieu <tieusharon@gmail.com>
2022-05-30 01:02:14 +0000

    Updated and did not use .includes.

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -9,7 +9,26 @@
 
 // Set.prototype.has() method returns a boolean to indicate whether an element with the specified value exists in the Set object or not.
 // In this case, we will set Set() to 'AEIOU' and 'aeiou' then assign it a new variable for it to be returned.
+// function isVowel(char) {
+//  var vowels = ['AaEeIiOoUu'];
+//  return vowels.includes(char);
+// }
+
 function isVowel(char) {
-  var vowels = new Set('AaEeIiOoUu');
-  return vowels.has(char);
+  var vowels = 'AaEeIiOoUu';
+  for (var i = 0; i < vowels.length; i++) {
+    var vowel = vowels[i];
+    if (vowel === char) {
+      return true;
+    }
+  } return false;
+
 }
+
+// return value should be a boolean (true or false);
+// write a function
+// assign a variable to the string of vowels (including uppercased characters)
+// note: array.includes() checks if parameters exist in the array. in this case, it will check if char is inside the array.
+// for loop to iterate through vowels
+// vowels[i] will be assigned var vowel
+// if vowel === char then return true.
