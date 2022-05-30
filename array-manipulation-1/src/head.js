20827f80859e87d2f8a219fb8a87af70456d80e1
sharon-tieu <tieusharon@gmail.com>
2022-05-30 18:55:54 +0000

    Added pseudo code.

--- array-manipulation-1/src/head.js
+++ array-manipulation-1/src/head.js
@@ -3,3 +3,9 @@ function head(array) {
   var firstItem = array[0];
   return firstItem;
 }
+
+// create a function to return the first element of the array
+// first element of the array is at index 0. This is written as array[0];
+// to return the first element of the array, assign it to a new variable and return it.
+// var firstItem = array[0];
+// then return.
