38d67d183c6659280dcfc621822ca84752feb67f
sharon-tieu <tieusharon@gmail.com>
2022-06-06 22:20:00 +0000

    Added notes.

--- javascript-this/student.js
+++ javascript-this/student.js
@@ -10,3 +10,13 @@ var student = {
     return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
   }
 };
+
+// getFull name: function () {
+// var fullName = this.firstName + ' ' + this.lastName;
+// return fullName;
+// }
+
+// getIntroduction: function () {
+// var introduction = 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+// return introduction;
+// }
