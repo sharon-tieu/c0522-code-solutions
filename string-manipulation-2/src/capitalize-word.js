e495299625d554ffc019cb5498ddfefc1dc89e1d
sharon-tieu <tieusharon@gmail.com>
2022-06-07 00:51:35 +0000

    In progress.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,4 +1,10 @@
 /* exported capitalizeWord */
+// establish a variable for an empty string
+// when given a word, we should go through each character (for loop)
+// we want the output to capitalize only at the first character- which is at index 0
+// everything else after index 0 will be lowercased.
+// uppercased character will be added to the rest of the characters.
+
 function capitalizeWord(word) {
   var uppercase = '';
   for (var i = 0; i < word.length; i++) {
@@ -10,9 +16,3 @@ function capitalizeWord(word) {
   }
   return uppercase;
 }
-
-// establish a variable for an empty string
-// when given a word, we should go through each character (for loop)
-// we want the output to capitalize only at the first character- which is at index 0
-// everything else after index 0 will be lowercased.
-// uppercased character will be added to the rest of the characters.
--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,9 @@
 /* exported capitalizeWords */
+
+// return a string. Means to set an empty string to a variable
+// goal: make a function that capitalizes every first word of a sentence
+
+// function capitalizeWords(string) {
+//  var firstLetterOfEachWord = '';
+//  string.charAt(0).toUpperCase() +
+// }
