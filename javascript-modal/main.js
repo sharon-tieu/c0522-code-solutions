98236d3b0565b679bdd390ca6775fd9e646f276f
sharon-tieu <tieusharon@gmail.com>
2022-06-07 04:15:12 +0000

    Ommited inline styling and added classes to remove and add.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,11 +1,12 @@
-var $button = document.querySelector('#button');
-var $modal = document.querySelector('#container');
-var $noButton = document.querySelector('#no-button');
+var button = document.querySelector('#button');
+var modal = document.querySelector('#container');
+var noButton = document.querySelector('#no-button');
 
-$button.addEventListener('click', function (event) {
-  $modal.style.display = 'block';
+button.addEventListener('click', function (event) {
+  modal.classList.remove('invisible');
+  modal.classList.add('visible');
 });
 
-$noButton.addEventListener('click', function (event) {
-  $modal.style.display = 'none';
+noButton.addEventListener('click', function (event) {
+  modal.classList.add('invisible');
 });
