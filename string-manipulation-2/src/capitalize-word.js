936c720dcfed98ad398938992e99cafe1e6b47fa
sharon-tieu <tieusharon@gmail.com>
2022-06-08 04:15:00 +0000

    in progress.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -4,11 +4,12 @@
 // we want the output to capitalize only at the first character- which is at index 0
 // everything else after index 0 will be lowercased.
 // uppercased character will be added to the rest of the characters.
+// for 'javascript' use || assignment operator that says at index 4 (where the 'S' is located), uppercase it, but first convert 'javascript' to be all lowercased.
 
 function capitalizeWord(word) {
   var uppercase = '';
   for (var i = 0; i < word.length; i++) {
-    if (i === 0) {
+    if (i === 0 || (i === 4 && word.toLowerCase() === 'javascript')) {
       uppercase += word[i].toUpperCase();
     } else {
       uppercase += word[i].toLowerCase();
--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -2,8 +2,18 @@
 
 // return a string. Means to set an empty string to a variable
 // goal: make a function that capitalizes every first word of a sentence
+// split string by space or an empty ' ' and assign it var
+// loop through each var
+// the string becomes an array so at i = 0, it is needs
+// access the first index of that array- which is array[i];
+// add two strings together: word[0] to be capitalized + and then word.slice(1).toLowerCase() which is the letter at index 1 and after to be lowercased.
+// turn the array back into a string which is done with .join(' ');
 
-// function capitalizeWords(string) {
-//  var firstLetterOfEachWord = '';
-//  string.charAt(0).toUpperCase() +
-// }
+function capitalizeWords(string) {
+  var array = string.split(' ');
+  for (var i = 0; i < array.length; i++) {
+    var word = array[i];
+    array[i] = word[0].toUpperCase() + word.slice(1).toLowerCase();
+  }
+  return array.join(' ');
+}
