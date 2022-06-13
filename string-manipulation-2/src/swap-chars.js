cb27a9cc2a716c03ca7f3e560c8dcb1ef6648084
sharon-tieu <tieusharon@gmail.com>
2022-06-13 17:01:20 +0000

    Must write last pseudo code.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,14 @@
 /* exported swapChars */
+function swapChars(firstIndex, secondIndex, string) {
+  var array = string.split('');
+  var firstChar = array[firstIndex];
+  var secondChar = array[secondIndex];
+  for (var i = 0; i < array.length; i++) {
+    if (i === firstIndex) {
+      array[i] = secondChar;
+    } else if (i === secondIndex) {
+      array[i] = firstChar;
+    }
+  }
+  return array.join('');
+}
