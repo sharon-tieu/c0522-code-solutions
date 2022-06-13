1d8cfc9eec9d307e7b5498cf1c1c84cc1cb68995
sharon-tieu <tieusharon@gmail.com>
2022-06-13 03:25:13 +0000

    Made loops for images and dots. Made forward increment. Must work on decrement arrow and corresponding circles.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,19 +1,81 @@
 // Global variables
 var leftArrow = document.querySelector('.left-arrow');
 var rightArrow = document.querySelector('.right-arrow');
-var circleImage1 = document.querySelector('#circle-image-1');
-var circleImage2 = document.querySelector('#circle-image-2');
-var circleImage3 = document.querySelector('#circle-image-3');
-var circleImage4 = document.querySelector('#circle-image-4');
-var circleImage5 = document.querySelector('#circle-image-5');
+var circleImage1 = document.querySelector('#circle-image-0');
+var circleImage2 = document.querySelector('#circle-image-1');
+var circleImage3 = document.querySelector('#circle-image-2');
+var circleImage4 = document.querySelector('#circle-image-3');
+var circleImage5 = document.querySelector('#circle-image-4');
+var image1 = document.querySelector('#image1');
+var images = ['./images/001.png', './images/004.png', './images/007.png', './images/025.png', './images/039.png'];
+var currentImage = 0;
+
+// To make images change, use setInterval (function () { })
+// Set all images we want in a carousel in an array [] to access them easily by index.
+// set currentImage to 0 so we have a reference point to what the "beginning" is
+
+// In setInterval, we write a function to change the image every second.
+// select <img> tag element, because we want to change its src value.
+// only want to change value of <img> src class attribute;
+// increment to the next index.
+// once setInterval loop reaches end of the array, then restart to i = 0;
+var carouselLoop = setInterval(function () {
+  var currentDot = document.querySelector('#circle-image-' + currentImage.toString());
+  if (currentImage === 0) {
+    var previousDot = document.querySelector('#circle-image-' + (images.length - 1).toString());
+  } else {
+    previousDot = document.querySelector('#circle-image-' + (currentImage - 1).toString());
+  }
+  currentDot.classList.add('fa-solid');
+  previousDot.classList.remove('fa-solid');
+  image1.src = images[currentImage];
+  currentImage++;
+  if (currentImage === images.length) {
+    currentImage = 0;
+  }
+}, 1000);
 
 // 'click' eventListeners:
-leftArrow.addEventListener('click', function (event) {
-  console.log('left arrow click detected!');
+
+// when user clicks an arrow, the carousel loop stops.
+// the image will continue to change accordingly to their index at whatever point the setInterval loop stopped at and will continue to change with each click.
+// this goes for pokemon images and dot images
+// to change images and dot, change the src
+rightArrow.addEventListener('click', function (event) {
+  clearInterval(carouselLoop);
+  currentImage = currentImage;
+  image1.src = images[currentImage];
+  var currentDot = document.querySelector('#circle-image-' + currentImage.toString());
+  if (currentImage === 0) {
+    var previousDot = document.querySelector('#circle-image-' + (images.length - 1).toString());
+  } else {
+    previousDot = document.querySelector('#circle-image-' + (currentImage - 1).toString());
+  }
+  previousDot.classList.remove('fa-solid');
+  currentDot.classList.add('fa-solid');
+  currentImage++;
+  if (currentImage === images.length) {
+    currentImage = 0;
+  }
 });
 
-rightArrow.addEventListener('click', function (event) {
-  console.log('right arrow click detected!');
+// Left Click Arrow
+leftArrow.addEventListener('click', function (event) {
+  clearInterval(carouselLoop);
+  currentImage = currentImage - 1;
+  image1.src = images[currentImage - 1];
+  var currentDot = document.querySelector('#circle-image-' + (currentImage - 1).toString());
+  // if (currentImage === images.length - 1) {
+  //   var previousDot = document.querySelector('#circle-image-0');
+  // } else {
+  //   previousDot = document.querySelector('#cicle-image-' + (currentImage - 1).toString());
+  // }
+  currentDot.classList.add('fa-solid');
+  // previousDot.classList.remove('fa-solid');
+  currentImage--;
+  if (currentImage === -1) {
+    currentImage = images.length - 1;
+  }
 });
 
 circleImage1.addEventListener('click', function () {
@@ -39,3 +101,8 @@ circleImage5.addEventListener('click', function () {
 // TIMER
 
 // view-swapping
+
+// Make a DOMContentLoaded f(x) because we want a function to run to loop the carousel.
+document.addEventListener('DOMContentLoaded', function () {
+  carouselLoop();
+});
