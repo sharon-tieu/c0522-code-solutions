49e71352a73c35a5879468271eb9b2b3f986d9d5
sharon-tieu <tieusharon@gmail.com>
2022-06-19 03:50:46 +0000

    Completed string-manipulation-2 exercise.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,4 +1,12 @@
 /* exported swapChars */
+
+// the firstIndex and secondIndex should be switched
+// string must be split at empty string
+// must find firstIndex and secondIndex when looping through the string
+// when i === firstIndex, then that array[i] will be the secondChar
+// when i === secondIndex, then that array[i] will be the firstChar
+// then return the array and join ''
+
 function swapChars(firstIndex, secondIndex, string) {
   var array = string.split('');
   var firstChar = array[firstIndex];
