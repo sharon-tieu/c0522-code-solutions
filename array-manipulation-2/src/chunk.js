2114acfc65326bf66680d7805f4b26039ea0d02f
sharon-tieu <tieusharon@gmail.com>
2022-06-20 06:56:46 +0000

    array-manipulation-2 Complete.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,22 @@
 /* exported chunk */
+// count = to number of things inside the sub-array
+// iterate through array
+// each element needs to be given a variable.
+
+function chunk(array, size) {
+  var parentArray = [];
+  var subArray = [];
+  if (size > array.length) {
+    return array;
+  }
+  for (var i = 0; i < array.length; i++) {
+    var element = array[i];
+    if (subArray.length === size) {
+      parentArray.push(subArray);
+      subArray = []; // the subArray should be reseted after array.length = size to make a new subArray set.
+    }
+    subArray.push(element);
+  }
+  parentArray.push(subArray); // pushes the next iteration that didn't get pushed yet
+  return parentArray;
+}
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,16 @@
 /* exported dropRight */
+
+// from the example:
+// array.length is 4
+// 4 - 2 = 2
+// remove 2
+function dropRight(array, count) {
+  var newArray = [];
+  if (count > array.length) {
+    return array;
+  }
+  for (var i = 0; i < array.length - count; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
