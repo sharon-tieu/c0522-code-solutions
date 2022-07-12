755365e3500ecff8f7cea0a58115c24fcae94c54
sharon-tieu <tieusharon@gmail.com>
2022-07-12 01:32:06 +0000

    Complete javascript-carousel exercise.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,11 +1,11 @@
 // Global variables
 var leftArrow = document.querySelector('.left-arrow');
 var rightArrow = document.querySelector('.right-arrow');
-var circleImage1 = document.querySelector('#circle-image-0');
-var circleImage2 = document.querySelector('#circle-image-1');
-var circleImage3 = document.querySelector('#circle-image-2');
-var circleImage4 = document.querySelector('#circle-image-3');
-var circleImage5 = document.querySelector('#circle-image-4');
+var circleImage0 = document.querySelector('#circle-image-0');
+var circleImage1 = document.querySelector('#circle-image-1');
+var circleImage2 = document.querySelector('#circle-image-2');
+var circleImage3 = document.querySelector('#circle-image-3');
+var circleImage4 = document.querySelector('#circle-image-4');
 var image1 = document.querySelector('#image1');
 var images = ['./images/001.png', './images/004.png', './images/007.png', './images/025.png', './images/039.png'];
 var currentImage = 0;
@@ -35,15 +35,9 @@ var carouselLoop = setInterval(function () {
   }
 }, 1000);
 
-// 'click' eventListeners:
-
-// when user clicks an arrow, the carousel loop stops.
-// the image will continue to change accordingly to their index at whatever point the setInterval loop stopped at and will continue to change with each click.
-// this goes for pokemon images and dot images
-// to change images and dot, change the src
 rightArrow.addEventListener('click', function (event) {
   clearInterval(carouselLoop);
-  currentImage = currentImage;
+  // currentImage = currentImage;
   image1.src = images[currentImage];
   var currentDot = document.querySelector('#circle-image-' + currentImage.toString());
   if (currentImage === 0) {
@@ -61,47 +55,68 @@ rightArrow.addEventListener('click', function (event) {
 
 leftArrow.addEventListener('click', function (event) {
   clearInterval(carouselLoop);
-  currentImage = currentImage - 1;
-  image1.src = images[currentImage - 1];
-  var currentDot = document.querySelector('#circle-image-' + (currentImage - 1).toString());
-  // if (currentImage === images.length - 1) {
-  //   var previousDot = document.querySelector('#circle-image-0');
-  // } else {
-  //   previousDot = document.querySelector('#cicle-image-' + (currentImage - 1).toString());
-  // }
-  currentDot.classList.add('fa-solid');
-  // previousDot.classList.remove('fa-solid');
+  // currentImage = currentImage;
   currentImage--;
   if (currentImage === -1) {
     currentImage = images.length - 1;
   }
+  image1.src = images[currentImage];
+  var currentDot = document.querySelector('#circle-image-' + currentImage.toString());
+  if (currentImage === images.length - 1) {
+    var previousDot = document.querySelector('#circle-image-0');
+  } else {
+    previousDot = document.querySelector('#circle-image-' + (currentImage + 1).toString());
+  }
+  previousDot.classList.remove('fa-solid');
+  currentDot.classList.add('fa-solid');
+});
+
+circleImage0.addEventListener('click', function () {
+  clearInterval(carouselLoop);
+  image1.src = images[0];
+  circleImage0.classList.add('fa-solid');
+  circleImage1.classList.remove('fa-solid');
+  circleImage2.classList.remove('fa-solid');
+  circleImage3.classList.remove('fa-solid');
+  circleImage4.classList.remove('fa-solid');
 });
 
 circleImage1.addEventListener('click', function () {
-  console.log('circle 1 click detected!');
+  clearInterval(carouselLoop);
+  image1.src = images[1];
+  circleImage0.classList.remove('fa-solid');
+  circleImage1.classList.add('fa-solid');
+  circleImage2.classList.remove('fa-solid');
+  circleImage3.classList.remove('fa-solid');
+  circleImage4.classList.remove('fa-solid');
 });
 
 circleImage2.addEventListener('click', function () {
-  console.log('circle 2 click detected!');
+  clearInterval(carouselLoop);
+  image1.src = images[2];
+  circleImage0.classList.remove('fa-solid');
+  circleImage1.classList.remove('fa-solid');
+  circleImage2.classList.add('fa-solid');
+  circleImage3.classList.remove('fa-solid');
+  circleImage4.classList.remove('fa-solid');
 });
 
 circleImage3.addEventListener('click', function () {
-  console.log('circle 3 click detected!');
+  clearInterval(carouselLoop);
+  image1.src = images[3];
+  circleImage0.classList.remove('fa-solid');
+  circleImage1.classList.remove('fa-solid');
+  circleImage2.classList.remove('fa-solid');
+  circleImage3.classList.add('fa-solid');
+  circleImage4.classList.remove('fa-solid');
 });
 
 circleImage4.addEventListener('click', function () {
-  console.log('circle 4 click detected!');
-});
-
-circleImage5.addEventListener('click', function () {
-  console.log('circle 5 click detected!');
-});
-
-// TIMER
-
-// view-swapping
-
-// Make a DOMContentLoaded f(x) because we want a function to run to loop the carousel.
-document.addEventListener('DOMContentLoaded', function () {
-  carouselLoop();
+  clearInterval(carouselLoop);
+  image1.src = images[4];
+  circleImage0.classList.remove('fa-solid');
+  circleImage1.classList.remove('fa-solid');
+  circleImage2.classList.remove('fa-solid');
+  circleImage3.classList.remove('fa-solid');
+  circleImage4.classList.add('fa-solid');
 });
